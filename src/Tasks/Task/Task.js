838de5220eb68f10ff8816b4143c367e80921bbd
nipun <nipun.goyal@oracle.com>
2020-05-15 17:45:38 +0530

    ADD NOS

--- src/Tasks/Task/Task.js
+++ src/Tasks/Task/Task.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Task.css';
 function Task(props) {
     return (
-        <div className="Task" onClick={props.delete} >{props.desc}</div >
+        <div className="Task" onClick={props.delete} >{props.KeyIndex}: {props.desc}</div >
     );
 };
 export default Task;
